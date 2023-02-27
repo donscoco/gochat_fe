954402cf4c470b65aafc99b52292fd44427dd24e
chendongxu <chendongxu@foxmail.com>
2023-02-27 16:47:52 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  publicPath:'./',
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8888',
+        changeOrigin: true,
+        ws: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+  // publicPath: process.env.NODE_ENV === 'production' ? '/' : './'
+ 
+}
